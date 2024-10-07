6ee1998371db0124f53e6b3a52630cd8799dc36d
Tymofii Andronik <andronik.1769@gmail.com>
2024-10-07 21:46:04 +0300

    eslint

--- src/services/paginatable.ts
+++ src/services/paginatable.ts
@@ -2,7 +2,7 @@
 import { Representable } from "../models/representable";
 
 export interface Paginatable<TEntity extends Representable> {
-  // eslint-disable-next-line no-unused-vars
+   
   getPaginated(pageNumber: number, pageSize: number): TEntity[];
   getTotalCount(): number;
 }
